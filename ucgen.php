7761b949b2da62b7f772fac263f78b02801e31d2
Winfamyss <106815919+Winfamyss@users.noreply.github.com>
2022-06-29 10:51:30 +0300

    Add files via upload

---
+++ ucgen.php
@@ -0,0 +1,19 @@
+<?php 
+
+function ucgen($s){
+      for($i=0;$i<=$s;$i++){
+    
+        $a=0; 
+          while($a<=$i){
+             $a++;
+            echo $a*0 ;
+       
+   }     
+     echo   "<br>";
+   } 
+
+}
+ucgen(15);
+
+
+?>
